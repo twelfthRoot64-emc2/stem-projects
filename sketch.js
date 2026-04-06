d3952d39ee5699848f26c941f1353fadf033b1a7
twelfthRoot64-emc2 <yohanvinay10@gmail.com>
2026-04-06 20:57:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,4 @@
-let x = 0;          // position
-let velocity = 2;   // constant speed
-
 function setup() {
-createCanvas(600, 400);
-}
-
-function draw() {
-background(220);
-
-// update position
-x += velocity;
-
-// reset when it goes off screen
-if (x > width) {
-x = 0;
-}
-
-// draw object
-circle(x, 200, 20);
+  createCanvas(600, 400);
+  background(255, 100, 0);
 }
