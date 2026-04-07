f06bf4a7b709ebf94761c991ef8ad239dc188301
twelfthRoot64-emc2 <yohanvinay10@gmail.com>
2026-04-07 16:49:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function windowResized(){
 
 function draw() {
 background(220);
-stroke('black');
+stroke('magenta');
 strokeWeight(5);
 line(0, height/2, width, height/2);
 line(width/2, 0, width/2, height);
