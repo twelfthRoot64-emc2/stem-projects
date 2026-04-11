a15e7a4586dd785c8ff2954a1480f87d9d1a47a4
twelfthRoot64-emc2 <yohanvinay10@gmail.com>
2026-04-11 12:49:20 +0530

    Update sketch.js
    Added velocity in y axis, the option to toggle between pause and play, and fixed some bugs.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,107 @@
+let canvas;
 let x = 0;
 let y = 0;
-let velocitySlider;
+let scale1 = 50;
+let velocitySliderX;
+let velocitySliderY;
 let button;
-let isRunning = true;
+let isPaused = true;
+let font;
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
-  velocitySlider = createSlider(-5, 5, 2);
-  button = createButton("Pause");
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.mousePressed(setPosition);
+  velocitySliderX = createSlider(-5, 5, 2);
+  velocitySliderY = createSlider(-5, 5, 2);
+  button = createButton("Play");
   button.mousePressed(toggleSim);
-  translate(width/2, height/2)
 }
 
 function toggleSim(){
-  isRunning = !isRunning;
+  isPaused = !isPaused;
+  
+  if(isPaused){
+    button.html("Play");
+  }
+  else{
+    button.html("Pause");
+  }
 }
 
-function mousePressed(){
-  x = mouseX;
-  y = mouseY;
+function setPosition(){
+  x = mouseX-width/2;
+  y = height/2 - mouseY;
 }
 
 function draw(){
   background(300);
+  translate(width/2, height/2);
+  scale(1, -1);
   
   stroke('black');
   strokeWeight(5);
-  line(0, height/2, width, height/2);
-  line(width/2, height, width/2, 0);
+  line(-width, 0, width, 0);
+  line(0, -height, 0, height);
   
-  let velocity = velocitySlider.value();
+  for(let i = -10; i <= 0; i++){
+    let xPos = i * scale1;
+    
+    strokeWeight(3);
+    line(xPos, -5, xPos, 5);
+    
+    push();
+    scale(1, -1);
+    strokeWeight(0.5);
+    text(i, xPos - 9, -10);
+    pop();
+  }
   
-  x += velocity;
+  for(let i = 1; i <= 10; i++){
+    let xPos = i * scale1;
+    
+    strokeWeight(3);
+    line(xPos, -5, xPos, 5);
+    
+    push();
+    scale(1, -1);
+    strokeWeight(0.5);
+    text(i, xPos - 5, -10);
+    pop();
+  }
   
-  if(x>width){
-    x = 0;
+  for(let i = 1; i <= 10; i++){
+    let yPos = i * scale1;
+    
+    strokeWeight(3); 
+    line(-5, yPos, 5, yPos);
+    
+    push();
+    scale(1, -1);
+    strokeWeight(0.5);
+    text(-i, -20, yPos + 5);
+    pop();
+    
+  }
+  
+  for(let i = -10; i <= -1; i++){
+    let yPos = i * scale1;
+    
+    strokeWeight(3);
+    line(-5, yPos, 5, yPos);
+    
+    push();
+    scale(1, -1);
+    strokeWeight(0.5);
+    text(-i, -20, yPos + 5);
+    pop();
+  }
+  
+  let velocityX = velocitySliderX.value();
+  let velocityY = velocitySliderY.value();
+  
+  if(!isPaused){
+  x += velocityX;
+  y += velocityY;
   }
   
   circle(x, y, 10)
